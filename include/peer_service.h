dffa73a4fd8ea07d20a5caf21b8f0da0c7631b9a
Vitor Enes <vitorenesduarte@gmail.com>
2017-11-12 18:51:30 +0000

    Pass wrapper to peer service, where data received from socket will be applied

--- include/peer_service.h
+++ include/peer_service.h
@@ -1,9 +1,101 @@
 #ifndef __PEER_SERVICE_H_INCLUDED__
 #define __PEER_SERVICE_H_INCLUDED__
 
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <pthread.h>
 #include <string>
 #include <iostream>
 
-void peer_service(const int& port);
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::string;
+using std::stringstream;
+
+const unsigned int MAX_DATAGRAM_SIZE = 65536;
+
+template<typename T>
+class Wrapper {
+public:
+  T *t_;
+  Wrapper(T* t) : t_(t) { }
+  virtual void apply(char* buf, int len) = 0;
+};
+
+template<typename T>
+struct ReaderArgs {
+  int port;
+  Wrapper<T>* w;
+};
+
+void diep(string msg) {
+  cerr << msg << endl;
+  exit(1);
+}
+
+template<typename T>
+void *reader(void *in);
+
+template<typename T>
+void peer_service(const int& port, Wrapper<T>* w) {
+  pthread_t tr;
+  ReaderArgs<T>* args = new ReaderArgs<T>;
+  args->port = port;
+  args->w = w;
+
+  int r = pthread_create(&tr, NULL, reader<T>, (void *) args);
+  if(r) diep("Error creating reader thread");
+}
+
+template<typename T>
+void *reader(void *in) {
+  struct ReaderArgs<T>* args = (ReaderArgs<T>*) in;
+  int r;
+  // open socket
+  int sock;
+  if((sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
+    diep("Error creating socket");
+
+  // bind ip address
+  struct sockaddr_in local_addr;
+  memset((char *) &local_addr, 0, sizeof(local_addr));
+  local_addr.sin_family = AF_INET;
+  local_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  local_addr.sin_port = htons(args->port);
+  r = bind(
+    sock,
+    (struct sockaddr *) &local_addr,
+    sizeof(local_addr)
+  );
+  if(r == -1) diep("Error binding address");
+
+  // listen new messages forever
+  char buf[MAX_DATAGRAM_SIZE];
+  struct sockaddr_in remote_addr;
+
+  while(1) {
+    int len = sizeof(remote_addr);
+    r = recvfrom(
+      sock,
+      buf,
+      MAX_DATAGRAM_SIZE,
+      0,
+      (struct sockaddr *) &remote_addr,
+      (socklen_t *) &len
+    );
+    if(r == -1) diep("Error reading from socket");
+
+    //char *ip_from = inet_ntoa(remote_addr.sin_addr);
+    //int port_from = ntohs(remote_addr.sin_port);
+    //cout << "Received " << buf << " from "
+    //     << ip_from << ":" << port_from << endl;
+    cout << "BEFORE APPLY" << endl;
+    args->w->apply(buf, len);
+    cout << "AFTER APPLY" << endl;
+  }
+}
 
 #endif
--- src/main.cc
+++ src/main.cc
@@ -4,8 +4,21 @@
 using std::cout;
 using std::endl;
 
+class HandoffWrapper : public Wrapper<int> {
+public:
+  HandoffWrapper(int* x) : Wrapper(x) { }
+  ~HandoffWrapper() { }
+  void apply(char* buf, int len) {
+    cout << *t_ << endl;
+    cout << buf << endl;
+    cout << len << endl;
+  }
+};
+
 int main() {
-  peer_service(3000);
+  int x = 3;
+  Wrapper<int>* wrapper = new HandoffWrapper(&x);
+  peer_service(3000, wrapper);
   sleep(1000);
   return 0;
 }
